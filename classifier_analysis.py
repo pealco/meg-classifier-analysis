a2bee34b88cdac426c244c379ab4abcd9649105e
Pedro Alcocer <pealco@gmail.com>
2010-04-28 14:07:26 -0400

    Adding code.

---
+++ classifier_analysis.py
@@ -0,0 +1,36 @@
+import tables
+import mdp
+from mvpa.suite import *
+
+def pca(data, cutoff=0.95):
+    """Expects data to be of shape (2, channels, trials).
+    Returns a list of conditions length of arrays of shape channels x trials."""
+    
+    data = hstack((data[0], data[1]))
+    
+    pcanode = mdp.nodes.PCANode(output_dim=cutoff, svd=True)
+    pcanode.train(data)
+    pcanode.stop_training()
+    
+    print "%d principal components were kept." % pcanode.output_dim
+    
+    pca_data = pcanode(data)
+    return hsplit(pca_data, 2)
+
+def classify(data):
+    """Expects data of shape (2, channels, trials)."""
+
+    pass
+
+def classifier_analysis(data_file):
+    file = tables.openFile(the_file)
+    raw_epochs = file.root.raw_data_epochs
+    conditions, samples, channels, trials = raw_epochs.shape
+    
+    accuracy = [classify(raw_epochs[:, sample, ...]) for sample in range(samples)]
+    
+    return accuracy
+    
+if __name__ == "__main__":
+    
+    classifier_analysis()
